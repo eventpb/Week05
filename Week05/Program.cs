81e558ecfeaec04000d1ba7568caf9c7bdfa1799
eventpb <59030195@kmitl.ac.th>
2017-09-08 10:15:07 +0700

    W05P02

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-public class GotoTest
+
+namespace ConsoleApp2
 {
-    static void Main(string[] args)
+    class CoffeeShop
     {
-        Console.WriteLine("Line 1");
-        line4:
-        Console.WriteLine("Line 4");
-        Console.WriteLine("Line 5");
-        Console.WriteLine("Line 2");
-        goto line9;
-        Console.WriteLine("Line 7");
-        Console.WriteLine("Line 8");
-        line9:
-        Console.WriteLine("Line 9");
-        
+        static void Main()
+        {
+            Console.WriteLine("Coffee sizes: 1=Small 2=Medium 3=Large");
+            Console.Write("Please enter your selection: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int cost = 0;
+            switch (n)
+            {
+                case 1:
+                    cost += 25;
+                    break;
+                case 2:
+                    cost += 25;
+                    goto case 1;
+                case 3:
+                    cost += 50;
+                    goto case 1;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} Bath.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
     }
 }
